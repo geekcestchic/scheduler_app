dfc6638a67249bdb24163263abea5808a28f2ca9
Alex Howard <alexhoward007@hotmail.com>
2015-02-12 14:05:57 +0000

    styling and fixed email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,6 +22,6 @@ class User < ActiveRecord::Base
   end
 
   validates :email, presence: true
-  validates :my_email_attribute, :email => true
+  validates :email, :email => true
   validates :password, presence: true
 end
